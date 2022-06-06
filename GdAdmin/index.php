90c48c8034fc91bd5cebcb4464690992165adc4f
ElTyChung <you@example.com>
2022-06-06 14:44:18 +0700

    push

--- GdAdmin/index.php
+++ GdAdmin/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +8,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <header>
         <h1>Admin-Website</h1>
@@ -14,7 +16,7 @@
     <div class="main">
         <nav>
             <ul class="menu">
-                <li ><a class="active" href="#">Quản lí sản phẩm</a></li>
+                <li><a class="active" href="#">Quản lí sản phẩm</a></li>
                 <li><a href="#">Quản lí loại sản phẩm</a></li>
                 <li><a href="#">Quản lí tài khoản</a></li>
                 <li><a href="#">Quản lí đơn hàng</a></li>
@@ -22,33 +24,38 @@
             </ul>
         </nav>
         <div class="content">
-            <?php 
-               
-                // include '../QlSanPham/Form_ShowSP.php';
-                include "../QlSanPham/Form_ShowSP.php"; 
+            <?php
+
+            // include '../QlSanPham/Form_ShowSP.php';
+            include "../QlSanPham/Form_ShowSP.php";
             ?>
         </div>
     </div>
 </body>
 
-<script  type="text/javascript">
+<script type="text/javascript">
     const $ = document.querySelector.bind(document);
     const $$ = document.querySelectorAll.bind(document);
     const li = $$(".menu >li>a");
     li.forEach((item) => {
-        item.addEventListener("click", () => { 
+        item.addEventListener("click", () => {
             $('.active').classList.remove("active");
             item.classList.add("active");
-            let tmp = item.textContent; 
+            let tmp = item.textContent;
             switch (tmp) {
                 case 'Quản lí sản phẩm':
                     $('.content').innerHTML = `<?php
                                                 include '../QlSanPham/Form_ShowSP.php'
                                                 ?>`
                     break;
-                case 'Quản lí loại sản phẩm':   
+                case 'Quản lí loại sản phẩm':
                     $('.content').innerHTML = `<?php
                                                 include '../QlLoaiSP/test.php'
+                                                ?>`
+                    break;
+                case 'Quản lí tài khoản':
+                    $('.content').innerHTML = `<?php
+                                                include '../QlTaiKhoan/QLTaiKhoan.php'
                                                 ?>`
                     break;
             }
@@ -56,9 +63,17 @@
     })
 
     function confirmDelete(delUrl) {
-            if (confirm("Bạn chắc chắn muốn xóa sản phẩm này?")) {
-                document.location = delUrl;         
-            }
+        if (confirm("Bạn chắc chắn muốn xóa sản phẩm này?")) {
+            document.location = delUrl;
         }
+    }
+
+    // function xacnhan(url){
+    //         if(confirm("Ban co muon xoa khong?")){
+    //             document.location=url;
+    //         }
+    //     }
+
 </script>
+
 </html>
